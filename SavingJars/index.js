b30b20d584b268d470d0d1dc03684868f8c888ad
Tri Nguyen <nguyenduongtri0503@gmail.com>
2019-11-15 10:28:34 +0700

    first commit

---
+++ SavingJars/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//set static folder
+app.use(express.static(__dirname + '/SavingJars'));
+
+//start server
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => (
+    console.log(`server is listening on port 
+    ${app.get('port')}')`)
+));
